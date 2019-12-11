bd013d0fbdbad5cc73702d403e66d8c839c82f0b
name <ebammo20@gmail.com>
2019-12-11 08:52:20 +0330

    main loop with a bug

--- main.py
+++ main.py
@@ -22,33 +22,42 @@ class IpHandler:
         :return: Boolean
         """
         # for i in range(6):
-        while True:
-            print('now pinging: %s' % ip)
-            status = self.ping(ip)
-            if status:
-                print('********************************** ', ip, ' is up ************************************')
-                # print(status)
-                time.sleep(60)
-            else:
-                print('********************************** ', ip, ' is down **********************************')
-                old_ip = ip
-                new_ip = self.change_server_with_ip(old_ip)
-                print('new ip: ', new_ip)
-                self.ips.append(new_ip)
-                index = self.ips.index(old_ip)
-                print('deleting: ', index)
-                del(self.ips[index])
-                print('ip counts: ', len(self.ips), self.ips)
-                # print('new ips: ', self.ips)
-                return new_ip
+        i = 0
+        print('now pinging: %s' % ip)
+        status = self.ping(ip)
+        if status:
+            print('********************************** ', ip, ' is up ************************************')
+            # print(status)
+            i += 1
+            print('#: ', i)
+            time.sleep(5)
+        else:
+            print('********************************** ', ip, ' is down **********************************')
+            i += 1
+            print('#: ', i)
+            old_ip = ip
+            new_ip = self.change_server_with_ip(old_ip)
+            print('new ip: ', new_ip)
+            # self.ips.append(new_ip)
+            # index = self.ips.index(old_ip)
+            # print('deleting: ', index)
+            # del(self.ips[index])
+            print('ip counts: ', len(self.ips), self.ips)
+            # print('new ips: ', self.ips)
+            # return {
+            #     'old_ip': old_ip,
+            #     'new_ip': new_ip,
+            # }
+            return [old_ip, new_ip]
 
     def make_threads(self):
         """make threads for each of ips"""
         print('making %s threads' % len(self.ips))
         threads_list = []
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            # results = executor.map(self.check_ip, self.ips)
-            results = [executor.submit(self.check_ip, ip) for ip in self.ips]
+            results = executor.map(self.check_ip, self.ips)
+            print('results: *************************************************', results)
+            # results = [executor.submit(self.check_ip, ip) for ip in self.ips]
             for f in concurrent.futures.as_completed(results):
                 res = f.result()
                 print('++++++++++++++ ', res)
@@ -68,32 +77,112 @@ class IpHandler:
 
     def make_t_hand(self):
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            t0 = executor.submit(self.check_ip, self.ips[0])
-            t1 = executor.submit(self.check_ip, self.ips[1])
+            # t0 = executor.submit(self.check_ip, self.ips[0])
+            # results = executor.map(self.check_ip, self.ips)
+            results = [executor.submit(self.check_ip, ip) for ip in self.ips]
+            for ip in self.ips:
+                print('my ip: ', ip)
+                index = self.ips.index(ip)
+                print('index: ', index)
+                f = executor.submit(self.check_ip, self.ips[index])
+                print(f)
+                result = f.result()
+                print(result)
+                if result:
+                    print(result)
+                    old_ip = result[0]
+                    new_ip = result[1]
+                    index = self.ips.index(old_ip)
+                    self.ips[index] = new_ip
+                    print(self.ips)
+
+                f = executor.submit(self.check_ip, self.ips[index])
+                result = f.result()
+                if result:
+                    print(result)
+                    old_ip = result[0]
+                    new_ip = result[1]
+                    index = self.ips.index(old_ip)
+                    self.ips[index] = new_ip
+                    print(self.ips)
+
+            # for f in concurrent.futures.as_completed(results):
+            # for f in results:
+            #     index = results.index(f)
+            #     result = f.result()
+            #     if result:
+            #         old_ip = result[0]
+            #         new_ip = result[1]
+            #         index = self.ips.index(old_ip)
+            #         self.ips[index] = new_ip
+            #         print(self.ips)
+            #
+            #     while True:
+            #         f = executor.submit(self.check_ip, self.ips[index])
+            #         result = f.result()
+            #         print('++++++++++++++++++++++++********', result)
+            #         # old_ip = result['old_ip']
+            #         # new_ip = result['new_ip']
+            #         if result:
+            #             old_ip = result[0]
+            #             new_ip = result[1]
+            #             index = self.ips.index(old_ip)
+            #             self.ips[index] = new_ip
+            #             print(self.ips)
+            #             executor.submit(self.check_ip, self.ips[index])
+
+    def make_one_thread(self):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            index = 1
+            t0 = executor.submit(self.check_ip, self.ips[index])
+            result = t0.result()
+            if result:
+                print(result)
+                old_ip = result[0]
+                new_ip = result[1]
+                index = self.ips.index(old_ip)
+                self.ips[index] = new_ip
+                print(self.ips)
+            while True:
+                t0 = executor.submit(self.check_ip, self.ips[index])
+                result = t0.result()
+                if result:
+                    print(result)
+                    old_ip = result[0]
+                    new_ip = result[1]
+                    index = self.ips.index(old_ip)
+                    self.ips[index] = new_ip
+                    print(self.ips)
+
+            # t0.add_done_callback(self._future_completed())
             # t2 = executor.submit(self.check_ip, self.ips[2])
             # t3 = executor.submit(self.check_ip, self.ips[3])
             # t4 = executor.submit(self.check_ip, self.ips[4])
-            if t0.result():
-                t0 = executor.submit(self.check_ip, t0.result())
-                if t0.result():
-                    t0 = executor.submit(self.check_ip, t0.result())
-                    if t0.result():
-                        t0 = executor.submit(self.check_ip, t0.result())
-            if t1.result():
-                t1 = executor.submit(self.check_ip, t1.result())
-                if t1.result():
-                    t1 = executor.submit(self.check_ip, t1.result())
-                    if t1.result():
-                        t1 = executor.submit(self.check_ip, '172.217.250.164')
+            # if t0.result():
+            #     t0 = executor.submit(self.check_ip, t0.result())
+            #     if t0.result():
+            #         t0 = executor.submit(self.check_ip, t0.result())
+            #         if t0.result():
+            #             t0 = executor.submit(self.check_ip, t0.result())
+            # if t1.result():
+            #     t1 = executor.submit(self.check_ip, t1.result())
+            #     if t1.result():
+            #         t1 = executor.submit(self.check_ip, t1.result())
+            #         if t1.result():
+            #             t1 = executor.submit(self.check_ip, '172.217.250.164')
 
-            self.thread_generator(executor, t0)
-            self.thread_generator(executor, t1)
+            # self.thread_generator(executor, t0)
+            # self.thread_generator(executor, t1)
             # self.make_t_hand()
 
     def thread_generator(self, executor, response):
         if response.result():
             th = executor.submit(self.check_ip, response.result())
+            # th.add_done_callback(lambda f: self._future_completed())
             self.thread_generator(executor, th)
+
+    def _future_completed(self):
+        print('call back finished.')
 
     def ping(self, ip):
         response = system('ping ' + ip)
@@ -119,6 +208,7 @@ def main():
     ip_handler.get_ips()
     # ip_handler.make_threads()
     ip_handler.make_t_hand()
+    # ip_handler.make_one_thread()
     # ping_ips()
 
 
